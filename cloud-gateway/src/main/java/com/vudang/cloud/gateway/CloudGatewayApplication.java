b19eff944809d4c0b11f4103c6921be645adab20
Vu Dang <danglegiavu@gmail.com>
2022-01-17 17:39:05 +0700

    update hystrix-dashboard

--- cloud-gateway/src/main/java/com/vudang/cloud/gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/vudang/cloud/gateway/CloudGatewayApplication.java
@@ -1,11 +1,16 @@
 package com.vudang.cloud.gateway;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+@EnableCircuitBreaker
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
--- cloud-gateway/src/main/java/com/vudang/cloud/gateway/FallbackController.java
+++ cloud-gateway/src/main/java/com/vudang/cloud/gateway/FallbackController.java
@@ -1,2 +1,18 @@
-package com.vudang.cloud.gateway;public class FallbackController {
+package com.vudang.cloud.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/user-service-fallback")
+    public String userServiceFallback(){
+        return "UserService is talking longer than expected. " + "Please try again!";
+    }
+
+    @GetMapping("/department-service-fallback")
+    public String departmentServiceFallback(){
+        return "DepartmentService is talking longer than expected. " + "Please try again!";
+    }
 }
